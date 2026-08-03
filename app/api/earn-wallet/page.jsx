0bf5b39863d3142a880ee97d573758e89310deba
gajarbotol <ssb43550@gmail.com>
2026-08-03 07:17:31 +0600

    Create page.jsx

---
+++ app/api/earn-wallet/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+
+import EarnWallet from './components/EarnWallet';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <EarnWallet />
+    </main>
+  );
+}
